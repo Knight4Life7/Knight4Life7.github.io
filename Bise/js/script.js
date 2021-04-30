bc2c620aa7d8e9abc71b3fa2fe33cd140e3f2f99
Knight4Life7 <79359534+Knight4Life7@users.noreply.github.com>
2021-04-30 21:59:25 +0300

    Add files via upload

---
+++ Bise/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".").mCustomScrollbar({
+        axis:"x", // horizontal scrollbar
+        setWidth: 1000,
+        setHeight: 300,
+        autoHideScrollbar: false,
+        alwaysShowScrollbar: 2
+    });
+    });
