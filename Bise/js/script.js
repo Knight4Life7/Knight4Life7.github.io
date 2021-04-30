9fadf36a04601c02ca88565fb57e7c625430db18
Knight4Life7 <79359534+Knight4Life7@users.noreply.github.com>
2021-04-30 21:57:47 +0300

    Delete Bise directory

--- Bise/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".").mCustomScrollbar({
-        axis:"x", // horizontal scrollbar
-        setWidth: 1000,
-        setHeight: 300,
-        autoHideScrollbar: false,
-        alwaysShowScrollbar: 2
-    });
-    });
